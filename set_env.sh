370ff56948781347c0026c0d469fa91abc719fe1
magedsopihy <magedsopihy@gmail.com>
2021-09-28 19:37:00 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=sopihydev
+export POSTGRES_PASSWORD=sopihydev
+export POSTGRES_HOST=postgress.cposnpe0q3kh.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=sopihy-microservices
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8080
